50285e355a9409ffa3ceb352f035358bbec235af
VaLeRo <valdirlealrocha@hotmail.com>
2024-05-28 19:04:41 -0300

    Atualização Aula 01
    Atualização Curso de C#

---
+++ CursoCSharpAula01.cs
@@ -0,0 +1,28 @@
+// AULA 01 - https://youtu.be/dVzJ3bx68FA?list=PLx4x_zx8csUglgKTmgfVFEhWWBQCasNGi
+// Curso Programação Completo C#
+// Na aula de hoje do curso programação C#, vamos baixar todo conteúdo necessário, 
+// instalar as ferramentas e configurar tudo para que possamos aprende a programar em C#.
+// C# é uma linguagem de programação, multiparadigma, desenvolvida pela Microsoft como 
+// parte da plataforma .NET. A sua sintaxe orientada a objetos foi baseada no C++ mas inclui 
+// muitas influências de outras linguagens de programação como Java.
+// Nosso curso de C# será dividido em duas grandes partes, a primeira parte iremos aprender 
+// toda lógica de programação usando a linguagem C# no Visual Studio Code, na segunda parte 
+// iremos partir para a parte gráfica, usando o Visual Studio Community para criar programar 
+// no padrão Windows, com formulários, botões, elementos interativos, relatórios e banco de dados.
+// Instalamos:
+// Run time - ndp481-x86-x64-allos-ptb.exe
+// O SDK do .NET - dotnet-sdk-8.0.300-win-x64.exe
+// Dev pack - ndp481-devpack-ptb.exe
+// Configuramos a variável de ambiente do compilador do C#
+// C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+// Programamos a saída de comandos no terminal
+using System; // biblioteca das definições do C#, entrada e saída, console, tecklado...
+
+// nossa primeira classe
+class Principal {
+  
+  static void Main() {
+    Console.Write("Primeiro programa C#...");
+  }
+
+}
